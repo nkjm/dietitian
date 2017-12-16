f93454b3fe4706416a498d6ede5409f0663979d6
Nakajima Kazuki <JP22274@w022571708172m.local>
2017-12-16 15:19:45 +0900

    Compile js

--- routes/oauth.js
+++ routes/oauth.js
@@ -16,7 +16,7 @@ const login = new Login({
     channel_id: process.env.LINE_LOGIN_CHANNEL_ID,
     channel_secret: process.env.LINE_LOGIN_CHANNEL_SECRET,
     callback_url: process.env.LINE_LOGIN_CALLBACK_URL,
-    scope: "openid profile phone email",
+    scope: "openid profile",
     bot_prompt: "normal"
 });
 
