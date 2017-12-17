ed412a86d98608d4751b142ae2579143e9a9ac7c
Nakajima Kazuki <JP22274@w022571708172m.local>
2017-12-17 23:23:33 +0900

    improving

--- service/food/edamam.js
+++ service/food/edamam.js
@@ -20,8 +20,7 @@ module.exports = class ServiceFoodEdamam {
     @return {Array.<food>} - Array of food object.
     */
     static search_food(text_ja){
-        let food = {};
-        let food_name = "";
+        let food_list = [];
         debug("Going to translate text...");
         return Promise.resolve().then((response) => {
             return googlet.translate(text_ja, "en");
@@ -41,59 +40,82 @@ module.exports = class ServiceFoodEdamam {
             if (response.body.parsed && response.body.parsed.length > 0){
                 // We have extracted result.
                 response.body.parsed.map((entity) => {
-                    if (!entity.quantity) entity.quantity = 0.3;
-                    if (!entity.measure) entity.measure = {uri: "http://www.edamam.com/ontologies/edamam.owl#Measure_kilogram"};
+                    let found_whole = false;
+                    if (response.body.hints && response.body.hints.length > 0){
+                        response.body.hints.map((hint) => {
+                            if (hint.food.label === entity.food.label){
+                                hint.measures.map((measure) => {
+                                    if (measure.label === "Whole"){
+                                        found_whole = true;
+                                    }
+                                })
+                            }
+                        })
+                    }
+                    if (!entity.quantity){
+                        if (found_whole){
+                            entity.quantity = 1;
+                        } else {
+                            entity.quantity = 0.1;
+                        }
+                    }
+                    if (!entity.measure){
+                        if (found_whole){
+                            entity.measure = {uri: "http://www.edamam.com/ontologies/edamam.owl#Measure_unit"};
+                        } else {
+                            {uri: "http://www.edamam.com/ontologies/edamam.owl#Measure_kilogram"};
+                        }
+                    }
                     ingredients.push({
                         quantity: entity.quantity,
                         measureURI: entity.measure.uri,
                         foodURI: entity.food.uri
                     });
-                    if (food_name === ""){
-                        food_name = entity.food.label;
-                    } else {
-                        food_name += ` and ${entity.food.label}`;
-                    }
+                    food_list.push({
+                        name: entity.food.label
+                    });
                 });
             } else if (response.body.hints || response.body.hints.length > 0){
                 let measureURI, quantity;
                 response.body.hints[0].measures.map((measure) => {
                     if (measure.label == "Whole"){
-                        measureURI = "http://www.edamam.com/ontologies/edamam.owl#Measure_unit";
                         quantity = 1;
+                        measureURI = "http://www.edamam.com/ontologies/edamam.owl#Measure_unit";
                     }
                 });
                 ingredients.push({
-                    quantity: quantity || 0.3,
+                    quantity: quantity || 0.1,
                     measureURI: measureURI || "http://www.edamam.com/ontologies/edamam.owl#Measure_kilogram",
                     foodURI: response.body.hints[0].food.uri
                 });
-                food_name = response.body.hints[0].food.label;
+                food_list.push({
+                    name: response.body.hints[0].food.label
+                });
             } else {
                 return null;
             }
 
             let endpoint = "https://api.edamam.com/api/food-database/nutrients";
             let url = endpoint + `?app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_APP_KEY}`;
-            let body = {
-                ingredients: ingredients
-            }
+
             debug(`Going to get nutrient info of following ingredients.`);
             debug(ingredients);
 
-            return request.postAsync({
-                url: url,
-                body: body,
-                json: true
-            });
-        }).then((response) => {
-            if (response === null){
+            let done_get_all_nutrition = [];
+            ingredients.map((ingr) => {
+                done_get_all_nutrition.push(request.postAsync({
+                    url: url,
+                    body: {ingredients: [ingr]},
+                    json: true
+                }));
+            })
+
+            return Promise.all(done_get_all_nutrition);
+        }).then((responses) => {
+            if (responses === null){
                 return null;
             }
-            debug(response.body);
-            food = {
-                db_type: "edamam",
-                calorie: response.body.calories
-            }
+
             const nutrient_codes = [
                 {code:"FAT", prop:"fat"},
                 {code:"CHOCDF", prop: "carb"},
@@ -101,20 +123,37 @@ module.exports = class ServiceFoodEdamam {
                 {code:"FIBTG", prop: "fiber"},
                 {code:"CHOLE", prop: "cholesterol"}
             ];
-            nutrient_codes.map((code) => {
-                if (response.body.totalNutrients[code.code]){
-                    food[code.prop] = Math.floor(response.body.totalNutrients[code.code].quantity);
-                }
+
+            let offset = 0;
+            responses.map((response) => {
+                food_list[offset].db_type = "edamam";
+                food_list[offset].calorie = response.body.calories;
+
+                nutrient_codes.map((code) => {
+                    if (response.body.totalNutrients[code.code]){
+                        food_list[offset][code.prop] = Math.floor(response.body.totalNutrients[code.code].quantity);
+                    }
+                });
+                offset++;
             });
 
             // Translate food name.
-            return googlet.translate(food_name, "ja");
+            let food_name_list = [];
+            food_list.map((food) => {
+                food_name_list.push(food.name);
+            });
+            return googlet.translate(food_name_list, "ja");
         }).then((response) => {
             if (response === null){
                 return [];
             }
-            food.name = response[0];
-            return [food];
+
+            let offset = 0;
+            response[0].map((translated_name) => {
+                food_list[offset].name = translated_name;
+                offset++;
+            })
+            return food_list;
         })
     }
 };
