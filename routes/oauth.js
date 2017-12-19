6c664dacd2939052479560e7044493413ae9bdd5
Nakajima Kazuki <JP22274@w022571708172m.local>
2017-12-19 11:09:46 +0900

    Update auth scope

--- routes/oauth.js
+++ routes/oauth.js
@@ -15,7 +15,7 @@ const login = new Login({
     channel_id: process.env.LINE_LOGIN_CHANNEL_ID,
     channel_secret: process.env.LINE_LOGIN_CHANNEL_SECRET,
     callback_url: process.env.LINE_LOGIN_CALLBACK_URL,
-    scope: "openid profile email phone",
+    scope: "openid profile",
     bot_prompt: "normal"
 });
 
